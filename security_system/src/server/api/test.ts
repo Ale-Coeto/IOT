d2fc272d8171bc928ed317057fa78e9cd807b90e
Alejandra Coeto <a01285221@tec.mx>
2023-11-01 11:39:59 -0600

    api

---
+++ security_system/src/server/api/test.ts
@@ -0,0 +1,36 @@
+import {
+    ApiGatewayManagementApiClient,
+    PostToConnectionCommand,
+  } from "@aws-sdk/client-apigatewaymanagementapi";
+
+  const domain = "b67qy1uxm9.execute-api.us-west-2.amazonaws.com/production/@connections";
+  const stage = "";
+  
+  export const handler = async (event:any) => {
+    const domain = event.requestContext.domainName;
+    const stage = event.requestContext.stage;
+    const connectionId = event.requestContext.connectionId;
+    const callbackUrl = `https://${domain}/${stage}`;
+    const client = new ApiGatewayManagementApiClient({ endpoint: callbackUrl });
+  
+    const requestParams = {
+      ConnectionId: connectionId,
+      Data: "Hello!",
+    };
+  
+    const command = new PostToConnectionCommand(requestParams);
+  
+    try {
+      await client.send(command);
+    } catch (error) {
+      console.log(error);
+    }
+  
+    return {
+      statusCode: 200,
+    };
+
+    
+  };
+
+  
